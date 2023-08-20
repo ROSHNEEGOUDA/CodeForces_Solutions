aca7a715a6a4a363b59dab6dfb17ff5ccefbb4fb
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2023-08-20 21:29:09 +0530

    Create AEscalatorConversations.cpp

---
+++ AEscalatorConversations.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define sz(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+void solve() 
+{
+    int n,m,k,H; cin >> n >> m >> k >> H;
+    int ans = 0;
+    forn(i, n) 
+    {
+        int x; cin >> x;
+        ans += (H != x) && abs(H - x) % k == 0 && abs(H-x) <= (m-1) * k;
+    }
+    cout << ans << endl;
+}
+int main() 
+{
+    int t;
+    cin >> t;
+ 
+    forn(tt, t) 
+    {
+        solve();
+    }
+}
